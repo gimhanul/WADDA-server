074729ca0386a5389806b77312c02ef5fcad2cae
gimhanul <gimhanuly@gmail.com>
2021-11-22 09:13:07 +0900

    Solve: get secret error

--- wadda/settings.py
+++ wadda/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
         'USER': get_secret("DB_USER"), # 데이터베이스에서 사용할 계정
         'PASSWORD': get_secret("DB_PW"), # 계정의 비밀번호
         'HOST': get_secret("DB_HOST"), # 데이테베이스 주소
-        'PORT': get_secret("DB_PW"), # 데이터베이스 포트, mysql 디폴트값은 3306
+        'PORT': get_secret("DB_PORT"), # 데이터베이스 포트, mysql 디폴트값은 3306
         # 이런 정보는 사람마다 배포 환경마다 다를 수 있으므로 setting.json에서 관리하는게 더 나아용!
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
